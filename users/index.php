60b6c12cf84bb4276e83fe5d87602713c6ba3f09
Gabriel Portes <gabriel-portes@hotmail.com>
2019-11-10 23:40:56 -0300

    Adding index.php and drink.php for my entity user

---
+++ users/index.php
@@ -0,0 +1,37 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+require_once('../classes/Api.php');
+
+$requestMethod = strtoupper($_SERVER['REQUEST_METHOD']);
+$body = json_decode(file_get_contents('php://input'), 1);
+$header = getallheaders();
+
+$uri = array_filter(
+    explode('/', $_SERVER['REQUEST_URI']),
+    function ($elem) {
+        return !empty($elem);
+    }
+);
+
+$class = array_pop($uri);
+
+$api = new Api($requestMethod, $body, $header);
+
+switch ($requestMethod) {
+    case 'GET':
+        $api->usersGet();
+        break;
+
+    case 'POST':
+        $api->usersPost();
+        break;
+
+    case 'PUT':
+        $api->usersPut();
+        break;
+
+    case 'DELETE':
+        $api->usersDelete();
+        break;
+}
